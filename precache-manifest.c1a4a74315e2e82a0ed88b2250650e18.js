7d9418f358e337aed499ef9782d6ea0d50bc01bd
tomni <tom.nicholon1000@gmail.com>
2019-01-22 21:31:45 +0700

    Updates

--- precache-manifest.c1a4a74315e2e82a0ed88b2250650e18.js
+++ precache-manifest.c1a4a74315e2e82a0ed88b2250650e18.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/runtime~main.3b961160.js"
   },
   {
-    "revision": "8f1af11cf1a9b5c9743f",
-    "url": "/Portfolio/static/js/main.8f1af11c.chunk.js"
+    "revision": "33bc54dcfa1cae38f18e",
+    "url": "/Portfolio/static/js/main.33bc54dc.chunk.js"
   },
   {
     "revision": "f03c696429a712e7b1b5",
     "url": "/Portfolio/static/js/1.f03c6964.chunk.js"
   },
   {
-    "revision": "8f1af11cf1a9b5c9743f",
-    "url": "/Portfolio/static/css/main.b3dccae9.chunk.css"
+    "revision": "33bc54dcfa1cae38f18e",
+    "url": "/Portfolio/static/css/main.81849575.chunk.css"
   },
   {
-    "revision": "3cdb5338086a06cbf927bba928805b8a",
+    "revision": "ec64a9740c522e41ffec8ca3b6288a23",
     "url": "/Portfolio/index.html"
   }
 ];
