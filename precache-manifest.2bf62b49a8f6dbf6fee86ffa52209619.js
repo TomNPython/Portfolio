727653a272f778a5c8d30aa23e8b004202d1d270
tomni <tom.nicholon1000@gmail.com>
2019-01-24 13:31:13 +0700

    Updates

---
+++ precache-manifest.2bf62b49a8f6dbf6fee86ffa52209619.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "fac460665858fa2d7aea",
+    "url": "/Portfolio/static/js/main.fac46066.chunk.js"
+  },
+  {
+    "revision": "904442774e444d1432b4",
+    "url": "/Portfolio/static/js/1.90444277.chunk.js"
+  },
+  {
+    "revision": "fac460665858fa2d7aea",
+    "url": "/Portfolio/static/css/main.7aa2e6d1.chunk.css"
+  },
+  {
+    "revision": "c7a22956377beec0fb3d26d7b568624f",
+    "url": "/Portfolio/index.html"
+  }
+];
